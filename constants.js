a4c1686a874597986378874576df77035170ea76
Marc Tan <marc.lester.tan@sap.com>
2013-12-01 11:54:35 +0800

    changed README

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 Object.defineProperty(exports, "ACCESS_TOKEN", {
-    value:        'ACCESS_TOKEN_HERE',
+    value:        'ACCESS_TOKEN',
     enumerable:   true,
     writable:     false,
     configurable: false
